f162cac2159fdc357d8bfe34eaea7d0a814b35b6
Simen Jensen <sim-jens@hotmail.com>
2017-02-06 02:29:15 +0100

    Add files via upload

---
+++ main/java/JSON_Interpreter.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Simen on 05.02.2017.
+ */
+public class JSON_Interpreter {
+    private String data;
+    public JSON_Interpreter(String data){
+        this.data = data;
+    }
+
+    public Tinder_Object updateTinder(){
+
+        return null;
+    }
+}
---
+++ main/java/Main.java
@@ -0,0 +1,30 @@
+import sx.blah.discord.api.ClientBuilder;
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.api.events.EventDispatcher;
+import sx.blah.discord.util.DiscordException;
+
+/**
+ * Created by Simen on 05.02.2017.
+ */
+public class Main {
+    public static IDiscordClient client; // The instance of the discord client.
+
+    public static void main(String[] args) {
+        login(args[0]);
+
+        EventDispatcher dispatcher = client.getDispatcher();
+        dispatcher.registerListener(new AnnotationListener(client));
+    }
+
+    public static void login(String token){
+        ClientBuilder builder = new ClientBuilder();
+        builder.withToken(token);
+
+        try{
+            client = builder.login();
+        } catch (DiscordException e){
+            System.err.println("Error occurred while logging in!");
+            e.printStackTrace();
+        }
+    }
+}
